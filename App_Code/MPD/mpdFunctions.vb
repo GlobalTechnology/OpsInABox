2b1a8a1b88bd5cee6b72048b799f94d9cd43fcb3
Jon Vellacott <JonVellacott@agape.org.uk>
2014-07-21 13:50:10 +0100

    Added GetExpenseBudget central function. Dmbl/Database chagnes

--- App_Code/MPD/mpdFunctions.vb
+++ App_Code/MPD/mpdFunctions.vb
@@ -73,7 +73,7 @@ Public Class mpdFunctions
     Public Shared Function getBudgetForStaffPeriod(ByVal StaffId As Integer, ByVal Period As String) As Double
         Dim d As New MPD.MPDDataContext
 
-        Dim q = From c In d.AP_mpdCalc_StaffBudgets Where c.StaffId = StaffId And (c.Status = StaffRmb.RmbStatus.Processed Or c.Status = StaffRmb.RmbStatus.Approved) Order By c.BudgetPeriodStart Descending
+        Dim q = From c In d.AP_mpdCalc_StaffBudgets Where c.StaffId = StaffId And (c.Status = StaffRmb.RmbStatus.Processed) Order By c.BudgetPeriodStart Descending
         Dim r = q.Where(Function(c) c.BudgetPeriodStart <= Period).OrderByDescending(Function(c) c.BudgetPeriodStart)
         If r.Count > 0 Then
             Return r.First.ToRaise
@@ -88,7 +88,24 @@ Public Class mpdFunctions
 
         Return 0
     End Function
+    Public Shared Function getExpenseBudgetForStaffPeriod(ByVal StaffId As Integer, ByVal Period As String) As Double
+        Dim d As New MPD.MPDDataContext
 
+        Dim q = From c In d.AP_mpdCalc_StaffBudgets Where c.StaffId = StaffId And (c.Status = StaffRmb.RmbStatus.Processed) Order By c.BudgetPeriodStart Descending
+        Dim r = q.Where(Function(c) c.BudgetPeriodStart <= Period).OrderByDescending(Function(c) c.BudgetPeriodStart)
+        If r.Count > 0 Then
+            Return r.First.TotalBudget
+        End If
+        If q.Count > 0 Then
+            Return q.First.TotalBudget
+        End If
+        q = From c In d.AP_mpdCalc_StaffBudgets Where c.StaffId = StaffId And (c.Status = StaffRmb.RmbStatus.Draft) Order By c.BudgetPeriodStart Descending
+        If q.Count > 0 Then
+            Return q.First.TotalBudget
+        End If
+
+        Return 0
+    End Function
     Public Shared Function getAverageMonthlyIncomeOver12Periods(ByVal StaffId As Integer) As Double
         Try
 
