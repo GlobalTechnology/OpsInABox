8dc6e5d9db0d9c37cfe9d062be83d1c4f1e88492
Trent Schaller <trent.schaller@agapefrance.org>
2015-09-08 11:27:25 +0200

    using label with translation

--- DesktopModules/AgapeConnect/Account/Account.ascx.cs
+++ DesktopModules/AgapeConnect/Account/Account.ascx.cs
@@ -66,7 +66,7 @@ namespace DotNetNuke.Modules.Account
         protected void Page_Load(object sender, EventArgs e)
         {
             openAddCountry.ToolTip = Translate("btnAddCountryTooltip");
-
+            lblNew.Text = Translate("lblNew");
             _startDate = FirstDayOfMonthFromDateTime(DateTime.Today.AddMonths(-12));
             _endDate = LastDayOfMonthFromDateTime(DateTime.Today);
            
