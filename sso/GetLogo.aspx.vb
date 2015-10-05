531e7a042aae38ce348031578070e40183906390
David VIOLLEAU <david.violleau@agapefrance.org>
2015-10-05 15:51:09 +0200

    Update from production: Added /sso folder

---
+++ sso/GetLogo.aspx.vb
@@ -0,0 +1,16 @@
+﻿
+Partial Class sso_GetLogo
+    Inherits System.Web.UI.Page
+
+    Private Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        Dim logoFile = DotNetNuke.Common.GetPortalSettings.HomeDirectory & DotNetNuke.Common.GetPortalSettings.LogoFile
+        'Response.Write(DotNetNuke.Services.FileSystem.FileManager.Instance.GetFileContent(DotNetNuke.Services.FileSystem.FileManager.Instance.GetFile(pi.LogoFile)))
+        Dim original As System.Drawing.Image = Bitmap.FromFile(Server.MapPath(logoFile))
+
+        Dim aspect = original.Width / original.Height
+
+        original = original.GetThumbnailImage(300, 300 / aspect, Nothing, Nothing)
+        original.Save(Response.OutputStream, System.Drawing.Imaging.ImageFormat.Jpeg)
+
+    End Sub
+End Class
