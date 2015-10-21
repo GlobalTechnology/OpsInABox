864189c67ab943a983e5db251954807030c5dd06
MayWorkman <may.workman@cru.org>
2015-10-21 10:56:26 +0200

    Upon testing could not get check to see if "acDocument" exists in the files system. Removed this functionality.

--- DesktopModules/AgapeConnect/Documents/Documents.ascx.vb
+++ DesktopModules/AgapeConnect/Documents/Documents.ascx.vb
@@ -53,7 +53,6 @@ Namespace DotNetNuke.Modules.AgapeConnect
             Dim rootFolderId As Integer
 
             If Not String.IsNullOrEmpty(Settings("RootFolder")) Then
-                AgapeLogger.Info(UserId, " Settings rootFolder " & Settings("RootFolder"))
                 rootFolderId = Settings("RootFolder")
             Else
 
@@ -63,15 +62,6 @@ Namespace DotNetNuke.Modules.AgapeConnect
 
                     'No rootNode found
                     If rootNode.Count = 0 Then
-
-                        'See if acDocuments directory exists in the file system
-                        If FolderManager.Instance.GetFolder(PortalId, DocumentsModuleRoot) Is Nothing Then
-                            AgapeLogger.Info(UserId, " acDocuments directory does not exist ")
-                            FolderManager.Instance.AddFolder(PortalId, DocumentsModuleRoot)
-
-                        Else
-                            AgapeLogger.Info(UserId, " acDocuments directory exists! ")
-                        End If
 
                         'Add the rootNode acDocuments in the database
                         Dim insert As New AP_Documents_Folder
@@ -91,7 +81,7 @@ Namespace DotNetNuke.Modules.AgapeConnect
 
                     LoadTree(rootNode.First)
 
-                End If
+                End If          'Page.IsPostBack
             End If
             Return rootFolderId
         End Function
@@ -168,6 +158,9 @@ Namespace DotNetNuke.Modules.AgapeConnect
                     ddlFiles.DataTextField = "FileName"
                     ddlFiles.DataValueField = "FileId"
                     ddlFiles.DataBind()
+                    AgapeLogger.Info(UserId, " Inside here ")
+                Else
+                    AgapeLogger.Info(UserId, " acDocuments directory does not exist!!!! ")
                 End If
                 Dim pages = TabController.GetPortalTabs(PortalId, TabId, False, False)
                 ddlPages.DataSource = pages.OrderBy(Function(x) x.TabName)
