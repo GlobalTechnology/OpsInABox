6980d219d8c40014df952bacd540dabb661e38dd
Trent Schaller <trent.schaller@agapefrance.org>
2015-10-28 17:17:58 +0100

    uploading files now.

--- App_Code/Documents/DocumentsController.vb
+++ App_Code/Documents/DocumentsController.vb
@@ -89,7 +89,7 @@ Public Class DocumentsController
             End Select
         End If
         If Not theFile Is Nothing Then
-            Select Case theFile.Extension
+            Select Case theFile.Extension.ToLower
                 Case "gif"
                     Return Path & "GIF.png"
                 Case "bmp"
@@ -132,10 +132,10 @@ Public Class DocumentsController
 #Region "Add/Edit"
 
     'Public Shared Sub InsertDocument(ByVal FileId As Integer, FileName As String, Author As String, Permissions As String)
-    Public Shared Sub InsertDocument(ByVal FileId As Integer, FileName As String, Author As String)
+    Public Shared Sub InsertDocument(ByVal FileId As Integer, FileName As String, Author As String, ByRef moduleSettings As System.Collections.Hashtable)
         Dim d As New DocumentsDataContext()
         Dim insert As New AP_Documents_Doc
-        insert.FolderId = 47
+        insert.FolderId = GetRootFolderId(moduleSettings)
         insert.FileId = FileId
         insert.DisplayName = FileName
         insert.Author = Author
@@ -204,84 +204,5 @@ End Class
 
 
 '    Return SearchItemCollection
-
-'End Function
-'Public Function GetIcon(ByVal FileId As Integer?, ByVal Folderid As Integer) As String
-'    If FileId Is Nothing Then
-'        Return "images/folder.png"
-'    Else
-'        Return GetFileIcon(FileId, 4)
-'    End If
-
-'End Function
-
-'Protected Function GetFileIcon(ByVal FileId As Integer?, ByVal LinkType As Integer, Optional IconId As Integer? = -1) As String
-
-
-'    If FileId Is Nothing Then
-'        Return "images/folder.png"
-'    End If
-'    Dim Path As String = "images/"
-'    Dim theFile = FileManager.Instance.GetFile(FileId)
-'    If Not IconId Is Nothing And IconId > 0 Then
-'        Return FileManager.Instance.GetUrl(FileManager.Instance.GetFile(IconId))
-'    End If
-
-'    If FileId = -2 Then
-'        Select Case LinkType
-'            Case 0 : Return Path & "URL.png"
-'            Case 1 : Return Path & "YouTube.png"
-'            Case 2 : Return Path & "GoogleDoc.png"
-'            Case 3 : Return Path & "Url.png"
-
-'        End Select
-'    End If
-
-'    If Not theFile Is Nothing Then
-
-
-
-'        Select Case theFile.Extension
-'            Case "gif"
-'                Return Path & "GIF.png"
-'            Case "bmp"
-'                Return Path & "BMP.png"
-'            Case "doc"
-'                Return Path & "DOC.png"
-'            Case "jpg"
-'                Return Path & "JPG.png"
-'            Case "mov"
-'                Return Path & "MOV.png"
-'            Case "mp3"
-'                Return Path & "MP3.png"
-'            Case "mp4"
-'                Return Path & "MP4.png"
-'            Case "mpg"
-'                Return Path & "MPG.png"
-'            Case "pdf"
-'                Return Path & "PDF.png"
-
-'            Case "png"
-'                Return Path & "PNG.png"
-'            Case "psd"
-'                Return Path & "PSD.png"
-'            Case "tiff"
-'                Return Path & "TIFF.png"
-'            Case "txt"
-'                Return Path & "TXT.png"
-'            Case "wav"
-'                Return Path & "WAV.png"
-'            Case "zip"
-'                Return Path & "ZIP.png"
-
-
-'            Case Else
-'                Return Path & "Blank.png"
-
-'        End Select
-
-'    End If
-'    Return "images/Blank.png"
-
 
 'End Function
