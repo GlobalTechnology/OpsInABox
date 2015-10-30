94cf1671da8ea05b1b3db6f664932fd370fcf04a
Trent Schaller <trent.schaller@agapefrance.org>
2015-10-30 17:04:52 +0100

    Added add link options
    Only the upload button is wired up...

--- DesktopModules/AgapeConnect/Documents/AddDocument.ascx.vb
+++ DesktopModules/AgapeConnect/Documents/AddDocument.ascx.vb
@@ -8,6 +8,16 @@ Namespace DotNetNuke.Modules.AgapeConnect.Documents
         Protected Sub Page_Init(sender As Object, e As System.EventArgs) Handles Me.Init
         End Sub
 
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+            If Not IsPostBack Then
+                Dim pages = TabController.GetPortalTabs(PortalId, TabId, False, False)
+                ddlPages.DataSource = pages.OrderBy(Function(x) x.TabName)
+                ddlPages.DataTextField = "TabName"
+                ddlPages.DataValueField = "TabId"
+                ddlPages.DataBind()
+            End If
+        End Sub
+
         Protected Sub btnUploadFiles_Click(sender As Object, e As System.EventArgs) Handles btnUploadFiles.Click
             Try
                 Dim folder As IFolderInfo
@@ -27,6 +37,15 @@ Namespace DotNetNuke.Modules.AgapeConnect.Documents
             End Try
             Response.Redirect(NavigateURL())
         End Sub
+
+        Protected Sub btnAddLink_Click(sender As Object, e As System.EventArgs) Handles btnNewLink.Click
+            Response.Redirect(NavigateURL())
+        End Sub
+        Protected Sub btnCancel_Click(sender As Object, e As System.EventArgs) Handles btnCancel.Click
+            Response.Redirect(NavigateURL())
+        End Sub
+
+
 
     End Class
 End Namespace
